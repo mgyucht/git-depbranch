770cbd0fa41972709d420885b3720c3569a4e070
Miles Yucht <miles@databricks.com>
2017-11-28 21:54:46 -0800

    Fix license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject git-depbranch "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Simple inter-dependent branches in Git."
+  :url "https://github.com/mgyucht/git-depbranch"
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main git-depbranch.core
   :aot [git-depbranch.core]
